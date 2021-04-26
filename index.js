2401ba1e0d07e7e2a85e80990b4462bf81e45be7
suey0128 <syui0128@gmail.com>
2021-04-26 13:16:25 -0700

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solutions in this file
+
+const nameList = [];
+
+function writeCards(nameList, birthday) {
+    const thankYouList = [];
+    for (let i=0; i<nameList.length; i+=1) {
+        thankYouList[i] = (`Thank you, ${nameList[i]}, for the wonderful surprise gift!`); 
+    }
+    return thankYouList;
+}
+
+let n; 
+
+function countDown(n) {
+    while (n>=0) {
+        console.log(n);
+        n = n-1;
+    }
+    return n;
+}
+
