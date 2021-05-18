535c7e55ece00d93d608fc7d89799f8c3f2e8cb0
suey0128 <syui0128@gmail.com>
2021-05-18 12:58:07 -0700

    commit tree 400e7db873f5ddcd15741cded1381359ba641f1b

--- index.js
+++ index.js
@@ -1,22 +1,38 @@
 // Code your solutions in this file
 
-const nameList = [];
+// const nameList = [];
 
-function writeCards(nameList, birthday) {
+// function writeCards(nameList, birthday) {
+//     const thankYouList = [];
+//     for (let i=0; i<nameList.length; i+=1) {
+//         thankYouList[i] = (`Thank you, ${nameList[i]}, for the wonderful surprise gift!`); 
+//     }
+//     return thankYouList;
+// }
+
+// let n; 
+
+// function countDown(n) {
+//     while (n>=0) {
+//         console.log(n);
+//         n = n-1;
+//     }
+//     return n;
+// }
+
+
+
+function  writeCards(array, event) {
     const thankYouList = [];
-    for (let i=0; i<nameList.length; i+=1) {
-        thankYouList[i] = (`Thank you, ${nameList[i]}, for the wonderful surprise gift!`); 
+    for (let i=0; i<array.length; i++) {
+        thankYouList[i] = `Thank you, ${array[i]}, for the wonderful ${event} gift!`;
     }
     return thankYouList;
 }
 
-let n; 
-
-function countDown(n) {
-    while (n>=0) {
-        console.log(n);
-        n = n-1;
+function countDown(i) {
+    while ( i >= 0 ) {
+        console.log(i);
+        i -=1;
     }
-    return n;
 }
-
